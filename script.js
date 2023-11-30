ff6ed222f6f2462fc634e4db8d0db5d5dbc20ed4
dunjans10 <91346284+dunjans10@users.noreply.github.com>
2023-11-30 10:36:57 +0100

    Added CV

--- script.js
+++ script.js
@@ -8,6 +8,34 @@ menuToggle.addEventListener("click", function (event) {
   navUl.classList.toggle("show");
 });
 
+
+const navItems = document.querySelectorAll("nav ul li a");
+
+navItems.forEach((item) => {
+  item.addEventListener("click", function (event) {
+    
+    event.preventDefault();
+
+    setTimeout(() => {
+      navUl.classList.remove("show");
+    }, 200);
+
+    const targetId = item.getAttribute("href").substring(1); 
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      const headerHeight = document.querySelector("header").offsetHeight;
+      const targetPosition = targetSection.offsetTop - headerHeight;
+
+      window.scrollTo({
+        top: targetPosition,
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+
 //My Services - Swiper
 
 const swiper = new Swiper('.swiper', {
